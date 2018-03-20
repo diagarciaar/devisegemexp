f9a2337ad48858edf6c5d0a54936be0618833ded
Diego Alejandro Garcia <diagarciaar@unal.edu.co>
2018-03-20 20:54:43 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
+# routes.rb File
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/index', to: "welcome#index"
-  root 'welcome#index'
-  
-  resources :todos do
-    resources :items
-  end
+ devise_for :users
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get 'welcome/index',to: 'welcome#index'
+ root 'welcome#index'
 end
